51e34db115ef797c4685465bf22ba48beecb0d4e
Luis Munoz <183163201+lpmunoz99@users.noreply.github.com>
2025-06-18 21:44:40 -0600

    Completed functionality for Library.
    Completed design, book display function, read status function, and delete function.

--- logic.js
+++ logic.js
@@ -1,21 +1,114 @@
 const myLibrary = [];
 
-function Book(title, id, author, pages, read){
+//Define the contructor for the books
+function Book(title, author, pages){
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
+    this.id = crypto.randomUUID();
     this.title = title;
-    this.id = id;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.read = false;
 }
 
-function addBooktoLibrary(title, author, pages, read){
-    const addBook = new Book(title, crypto.randomUUID(), author, pages, read);
+//Function to add information to the object book
+function addBooktoLibrary(title, author, pages){
+    const addBook = new Book(title, author, pages);
     myLibrary.push(addBook);
 }
 
-addBooktoLibrary('prueba','ss','qq','qq');
+//Function to change read status
+Book.prototype.readStatus = function (){
+    if(this.read === false){
+        this.read = true;
+    } else {
+        this.read = false;
+    }
+};
 
-console.log(myLibrary);
+//Variables to select all the components that are needed
+const container = document.querySelector('.container');
+const content = document.querySelector('.container-content');
+const contentText = document.querySelector('.content-text');
+const dialog = document.querySelector('dialog');
+const btn = document.querySelector('.btn-book');
+const btnSubmit = document.querySelector('.btn-submit');
+const btnCancel = document.querySelector('.btn-cancel');
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+const pagesInput = document.getElementById('pages');
+
+contentText.style.fontStyle = "italic";
+
+//Function to open the dialog and enter the book information
+btn.addEventListener('click', () => {
+    dialog.showModal();
+});
+
+//Function to close the dialog and reset inputs
+btnCancel.addEventListener('click', () => {
+    dialog.close();
+    titleInput.value = "";
+    authorInput.value = "";
+    pagesInput.value = "";
+});
+
+//Function to submit the form information to display the books
+btnSubmit.addEventListener('click', () => {
+
+        //Clean display to add books
+        if(myLibrary.length === 0){
+            content.innerText = "";
+        }
+
+        //Display the books
+        addBooktoLibrary(titleInput.value, authorInput.value, pagesInput.value);
+        const newDiv = document.createElement('div');
+        newDiv.className = 'book';
+        for (let i = 0; i < myLibrary.length; i++) {
+            newDiv.dataset.id = myLibrary[i].id;
+            newDiv.innerText = "Title: " + myLibrary[i].title + "\n Author: " + myLibrary[i].author + "\n Pages: " + myLibrary[i].pages + "\n Have you read it?" + "\n";
+        }
+        content.appendChild(newDiv);
+        const btnStatus = document.createElement('button');
+        btnStatus.className = 'btn-status';
+        btnStatus.innerText = "Not read";
+
+        //Change button display based on read value
+        btnStatus.addEventListener('click', () => {
+            for (let i = 0; i < myLibrary.length; i++) {
+                myLibrary[i].readStatus();
+                if(myLibrary[i].read === true){
+                    btnStatus.innerText = "Read";
+                    btnStatus.style.backgroundColor = "green";
+                } else {
+                    btnStatus.innerText = "Not read";
+                    btnStatus.style.backgroundColor = "red";
+                }
+            }
+        });
+        newDiv.appendChild(btnStatus);
+        titleInput.value = "";
+        authorInput.value = "";
+        pagesInput.value = "";
+
+        //delete button functionality
+        const deleteButton = document.createElement('button');
+        deleteButton.className = "delete-book";
+
+        //search id and then index to properly delete book
+        deleteButton.addEventListener('click', () => {
+            let index = myLibrary.map (element => {
+                return element.id;
+            }).indexOf(newDiv.dataset.id);
+            myLibrary.splice(index, 1);
+            if(newDiv){
+                newDiv.remove();
+            }
+            if(myLibrary.length === 0){
+                content.appendChild(contentText);
+            }
+        });
+        newDiv.appendChild(deleteButton);
+});
